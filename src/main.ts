12b1c463cb6677dfdbb60e8b1e7e667a4903b418
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-03 14:43:35 +0200

    Add and first useage of creatElements function

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import './style.css';
+import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
+const mainElement = createElement('main', {
+  childElements: [
+    createElement('h1', {
+      innerHTML: 'Rick <span class="headerSpan">and</span> Morty',
+    }),
+    createElement('input', {
+      placeholder: 'Search for a character',
+      className: 'searchbar',
+    }),
+    createElement('p', { innerHTML: 'Test text für Font' }),
+  ],
+});
+// const mainElement = document.createElement('main');
 
-const mainElement = document.createElement('main');
+// const title = document.createElement('h1');
 
-const title = document.createElement('h1');
-title.innerText = 'Rick and Morty';
+// title.innerHTML = `Rick <span class="headerSpan">and</span>  Morty`;
+// const text = document.createElement('p');
+// text.innerText = 'Test text für Font';
 
-mainElement.append(title);
+// mainElement.append(title, text);
 
 if (app !== null) {
   app.append(mainElement);
