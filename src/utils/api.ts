4920ac5dab2ad1828308c3f9c76d196ccbba33e9
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-05 15:39:33 +0200

    Add export function for importing data from API

---
+++ src/utils/api.ts
@@ -0,0 +1,27 @@
+//--------------- Import ---------------
+
+import type { allCharactersApi, Character } from '../types';
+
+//--------------- Using an API ---------------
+
+export async function getCharacters(): Promise<Character[]> {
+  const response = await fetch('https://rickandmortyapi.com/api/character');
+  const data: allCharactersApi = await response.json();
+  const characters = data.results;
+  console.log(characters);
+
+  //--------------- We only want to get what we need ---------------
+
+  const formattCharacters: Character[] = characters.map((character) => {
+    const formattCharacter: Character = {
+      name: character.name,
+      image: character.image,
+      status: character.status,
+      species: character.species,
+      location: character.location.name,
+      origin: character.origin.name,
+    };
+    return formattCharacter;
+  });
+  return formattCharacters;
+}
