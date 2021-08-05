7b83535990878fa8e858b1a18a45ce66b744bf10
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-05 15:35:44 +0200

    Add new types for characters from API

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+//--------------- All my Types i want to export ---------------
+
 export type Character = {
   name: string;
   image: string;
@@ -5,4 +7,36 @@ export type Character = {
   species: string;
   location: string;
   origin: string;
+};
+
+export type CharacterApi = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+
+  origin: {
+    name: string;
+    url: string;
+  };
+
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+export type allCharactersApi = {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  };
+  results: CharacterApi[];
 };
